dc825bb6b65082341c54e8d82c56b3cd2b10c3de
razetime <rraghu.11502@gmail,com>
2023-12-13 22:27:00 +0800

    export image sequences

--- cbqn-80.c
+++ cbqn-80.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include "../cbqn/include/bqnffi.h"
 #include <SDL2/SDL.h>
+#include <SDL_image.h>
+#include <string.h>
+
 
 #define WINDOW_WIDTH 240
 #define WINDOW_HEIGHT 136
@@ -9,8 +12,20 @@ uint32_t c[]={0xff1a1c2c, 0xff5d275d, 0xffb13e53, 0xffef7d57, 0xffffcd75,
   0xffa7f070, 0xff38b764, 0xff257179, 0xff29366f, 0xff3b5dc9, 0xff41a6f6, 
   0xff73eff7, 0xfff4f4f4, 0xff94b0c2, 0xff566c86, 0xff333c5};
 
+void save_texture(const char* file_name, SDL_Renderer* renderer, SDL_Texture* texture) {
+    SDL_Texture* target = SDL_GetRenderTarget(renderer);
+    SDL_SetRenderTarget(renderer, texture);
+    int width, height;
+    SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+    SDL_Surface* surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
+    SDL_RenderReadPixels(renderer, NULL, surface->format->format, surface->pixels, surface->pitch);
+    IMG_SavePNG(surface, file_name);
+    SDL_FreeSurface(surface);
+    SDL_SetRenderTarget(renderer, target);
+}
+
 int main(int argc, char* argv[]) {
-  if(argc==1){puts("Usage: cbqn-80 file.bqn [framecount [out.jpg/gif]]");
+  if(argc==1){puts("Usage: cbqn-80 file.bqn [framecount [name]]");
     exit(0);
   }
   FILE* f=fopen(argv[1],"r");
@@ -23,7 +38,8 @@ int main(int argc, char* argv[]) {
     if(b)fread(b,sizeof(char),l,f);
     fclose(f);
   }
-  float m=100;
+  float m=30;
+  bool e=argc>3;
   if(argc>2) m=atof(argv[2]);
    
   bqn_init();
@@ -45,8 +61,10 @@ int main(int argc, char* argv[]) {
 
   int l=SDL_GetTicks();
   while(k<m){
-    if(SDL_GetTicks()-l<33) continue; // desired fps is around 30.
-    l=SDL_GetTicks();
+//    if(!e){
+      if(SDL_GetTicks()-l<33) continue; // desired fps is around 30.
+      l=SDL_GetTicks();
+  //  }
     x=bqn_call2(g,w,x);
     uint32_t* a=malloc(WINDOW_HEIGHT*WINDOW_WIDTH*sizeof(uint32_t)); 
     bqn_readI32Arr(x,a);
@@ -55,6 +73,11 @@ int main(int argc, char* argv[]) {
     SDL_UpdateTexture(Screen,NULL,a,WINDOW_WIDTH*sizeof(uint32_t));
     SDL_RenderCopy(renderer,Screen,NULL,NULL);
     SDL_RenderPresent(renderer);
+    if(e) {
+      char y[strlen(argv[3])+5];
+      sprintf(y,"%s%03i.png",argv[3],(int)k);
+      save_texture(y,renderer,Screen);
+    }
     k+=1;
     w=bqn_makeF64(k);
   }
--- makefile
+++ makefile
@@ -1,4 +1,6 @@
 all: build
 
 build:
-	gcc cbqn-80.c $(shell pkg-config --cflags --libs sdl2) -lcbqn -o cbqn-80
+	gcc cbqn-80.c $(shell pkg-config --cflags --libs sdl2) \
+	  $(shell pkg-config --cflags --libs SDL2_image) \
+	  -lcbqn -o cbqn-80
